f01568be1b2e121d3670fc83595bec0ed2a50a16
Rinzlertes <tesejz@gmail.com>
2024-06-11 00:33:07 +0300

    simple fetch and add axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,52 @@
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-} from '@tanstack/react-query'
+import { useQuery,
+  //  useMutation,
+  //   useQueryClient
+   } from "@tanstack/react-query";
 
+
+
+const getData = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return response.json();
+};
+
+interface DataProps {
+  userId: number,
+  id: number,
+  title: string,
+  body: string
+}
 
 function App() {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["posts"],
+    queryFn: getData,
+  });
 
   return (
     <>
-      <div>
-      
+      <div
+        style={{
+          padding: `24px`,
+        }}
+      >
+        {isLoading ? (
+          <h3>loading...</h3>
+        ) : (
+          <>
+            {data.map((item: DataProps) => {
+              return (
+                <div key={item.id}>
+                  <p>{item.title}</p>
+                </div>
+              );
+            })}
+          </>
+        )}
+        {error && <span>ERROR</span>}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
